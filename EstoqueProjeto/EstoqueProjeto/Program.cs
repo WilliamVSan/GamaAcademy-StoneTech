759dbd8441651aec403bd8b2563c845265986ee2
WilliamVSan <williamvcontato@gmail.com>
2022-09-06 16:53:47 -0300

    @WilliamVSan [Main: Adicionando projeto de estoque proposto pelo professor.]

---
+++ EstoqueProjeto/EstoqueProjeto/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EstoqueProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variavel do loop.
+            bool continuar = true;
+
+            //Adicionar novos produtos.
+            AdicionarProduto adicionar = new AdicionarProduto();
+            //Listar produtos.
+            ListarProduto listarProduto = new ListarProduto();
+            //Barra de carregamento.
+            Carregando barraDeCarregamento = new Carregando();
+            //Criando lista de produtos.
+            List<Produto> listaProdutos = new List<Produto>();
+            //Loop para continuar no sistema.
+            while (continuar)
+            {
+
+                Console.Clear();
+                //Função para mudar a cor das letras.
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("|  ||  ||  | Bem vindo ao gerenciamento de PRODUTOS |  ||  ||  |\n");
+
+                Console.WriteLine("Pressione a tecla correspondente para acessar uma das opções:");
+                Console.ResetColor();
+                Console.Write("L - Listar produtos\n" +
+                              "A - Adicionar novo produto\n" +
+                              /*"S - Listar compras\n" +
+                              "S - Listar vendas\n" +*/
+                              "S - Listar categorias\n" +
+                              "E - Editar produto\n\n");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Pressione 'Q' para sair.\n");
+                Console.ResetColor();
+                Console.ResetColor();
+
+                //Variavel pressione a tecla.
+                var input = Console.ReadKey();
+                switch (input.Key)
+                {
+                    case ConsoleKey.L:
+                        Console.Clear();
+                        listarProduto.Listando(listaProdutos);
+                        break;
+
+                    case ConsoleKey.V:
+                        Console.Clear();
+                        listarProduto.ListandoVendas();
+                        break;
+
+                    case ConsoleKey.C:
+                        Console.Clear();
+                        listarProduto.ListandoCompras();
+                        break;
+
+                    case ConsoleKey.A:
+                        Console.Clear();
+                        adicionar.CriarProduto();
+                        break;
+
+                    case ConsoleKey.S:
+                        Console.Clear();
+                        listarProduto.ListandoCategoria();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nPressione qualquer tecla para voltar");
+                        Console.ReadKey();
+                        break;
+
+                    case ConsoleKey.E:
+                        Console.Clear();
+                        adicionar.EditarProduto();
+                        break;
+
+                    case ConsoleKey.Q:
+                        continuar = false;
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Opção inválida");
+                        Thread.Sleep(1500);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ EstoqueProjeto/EstoqueProjeto/Teste.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueProjeto
+{
+    public class Teste
+    {
+        public void Testando()
+        {
+            using (var db = new EstoqueEntities())
+            {
+                Categoria categoria = new Categoria()
+                {
+                    NomeCategoria = "Escritorio",
+                    Descricao = "Materiais para escritorio",
+                    Tipo = "Tinta"
+                };
+                db.Categorias.Add(categoria);
+                db.SaveChanges();
+            }
+        }
+    }
+}
